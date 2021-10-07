902d29d7a228748f8c93b4c8c7388796e2996f58
Michael Wong <michaelwong1391@gmail.com>
2021-10-07 15:30:01 -0400

    Updates

--- src/Components/Video.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Video({video}) {
-   return (
-      <div>
-         <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameBorder="0" allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-      </div>
-   )
-}
