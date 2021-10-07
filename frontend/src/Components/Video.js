3fa87594577111263a31e0f67aeb1d3c83c871a7
Michael Wong <michaelwong1391@gmail.com>
2021-10-07 15:53:23 -0400

    added comments to frontend and general clean up of code

--- frontend/src/Components/Video.js
+++ frontend/src/Components/Video.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Video({video}) {
+   // Renders YouTube embed video
    return (
       <div>
          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameBorder="0" allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
--- frontend/src/Components/Videos.js
+++ frontend/src/Components/Videos.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Video from './Video'
 
 export default function Videos({videos}){
+   // Renders a video component 
    return (
       videos.map(video => {
          return <Video key={video} video={video} />;
